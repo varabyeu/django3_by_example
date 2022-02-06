53b6069012da1ec3dc0a9e68e9188de02e6c35fc
Varabyeu <varabyeu.v@yandex.by>
2022-02-06 23:11:24 +0300

    First chapter done. Views, html, pagination"

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -37,6 +38,13 @@ class Post(models.Model):
 
 	class Meta:
 		ordering = ('-publish',)
+
+	def get_absolute_url(self):
+		return reverse('blog:post_detail', args=[
+			self.publish.year,
+			self.publish.month,
+			self.publish.day,
+			self.slug])
 
 	def __str__(self):
 		return self.title
