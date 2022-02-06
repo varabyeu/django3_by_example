0c4d8a9f533754b90baaaf7a222241f01e6eea35
Varabyeu <varabyeu.v@yandex.by>
2022-02-06 21:36:53 +0300

    Comitt customized admin panel

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(
+			PublishedManager,
+			self
+		).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
 	STATUS_CHOICES = (
 		('draft', 'Draft'),
@@ -23,9 +32,14 @@ class Post(models.Model):
 		choices=STATUS_CHOICES,
 		default='draft'
 	)
+	objects = models.Manager()
+	published = PublishedManager()
+
 	class Meta:
 		ordering = ('-publish',)
 
 	def __str__(self):
 		return self.title
-# Create your models here.
+
+
+
