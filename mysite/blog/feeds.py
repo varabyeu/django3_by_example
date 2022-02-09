2d067d50d66c18ea5269db9c27594750534e7284
Varabyeu <varabyeu.v@yandex.by>
2022-02-09 17:00:11 +0300

    add RSS feed

---
+++ mysite/blog/feeds.py
@@ -0,0 +1,20 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse_lazy
+
+from blog.models import Post
+
+
+class LatestPostFeed(Feed):
+    title = 'My blog'
+    link = reverse_lazy('blog:post_list')
+    description = 'New post of my blog'
+
+    def items(self):
+        return Post.published.all()[:5]
+
+    def items_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return truncatewords(item.body, 30)
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostFeed
 
 app_name = 'blog'
 
@@ -9,5 +10,6 @@ urlpatterns = [
 	# path('', views.PostListView.as_view(), name='post_list'),
 	path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail,
 		 name='post_detail'),
-	path('<int:post_id>/share/', views.post_share, name='post_share')
+	path('<int:post_id>/share/', views.post_share, name='post_share'),
+	path('feed/', LatestPostFeed(), name='post_feed')
 ]
